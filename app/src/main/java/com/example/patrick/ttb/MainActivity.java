beb9e1c189e513d7bf0b11979973ac60e4b9221b
Aquilegia5 <pat.niessl@web.de>
2019-04-03 04:48:48 +0200

    Initial commit

---
+++ app/src/main/java/com/example/patrick/ttb/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.patrick.ttb;
+
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private CardView loginButt;
+    private EditText user, pass;
+    private int counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Login Felder
+        user = (EditText) findViewById(R.id.loginUname);
+        pass = (EditText) findViewById(R.id.loginPass);
+
+        // CardViews
+        loginButt = (CardView) findViewById(R.id.loginButton);
+
+        // Add Listeners
+        loginButt.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+
+        switch(v.getId()) {
+            case R.id.loginButton:
+                String inUser, inPass;
+                inUser = user.getText().toString();
+                inPass = pass.getText().toString();
+
+                if(5-counter == 0){
+                    //alert
+                    finish();
+                    System.exit(1);
+                }
+
+                if(inUser.equals("A") && inPass.equals("L")){
+                    i = new Intent(this, Calendar.class);
+                    startActivity(i);
+                } else {
+                    counter++;
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setMessage((5-counter) + " tries left.");
+                    builder.show();
+                }
+
+                user.setText("");
+                pass.setText("");
+                break;
+            default: break;
+        }
+    }
+}
